c6e35441b841315a45c50fa3e7ee985baf671fa4
Bhargav <vyasb1503@gmail.com>
2026-01-15 23:59:26 +0530

    coinage in the  eureka service

--- library/src/main/java/com/tka/LibraryApplication.java
+++ library/src/main/java/com/tka/LibraryApplication.java
@@ -1,14 +1,14 @@
-////package com.tka;
-////
-////import org.springframework.boot.SpringApplication;
-////import org.springframework.boot.autoconfigure.SpringBootApplication;
-//a//
-////@SpringBootApplication
-////
-////public class LibraryApplication {
-////
-////	public static void main(String[] args) {
-////		SpringApplication.run(LibraryApplication.class, args);
-////	}
-////
-////}
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
